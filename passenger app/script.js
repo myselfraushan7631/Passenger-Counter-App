d2e5c81a1a78113240fc2deb9cfa7b3bf4d07c09
Raushan Kumar <131746306+myselfraushan7631@users.noreply.github.com>
2024-09-26 08:02:10 +0530

    Add files via upload

---
+++ passenger app/script.js
@@ -0,0 +1,15 @@
+ //document.getElementById("count-ele").innerText=6;//
+ //let bonuspoint=50;
+ //console.log(bonuspoint);
+ //bonuspoint=bonuspoint+50;
+ //console.log(bonuspoint);
+ //bonuspoint=bonuspoint-75;
+ //console.log(bonuspoint);
+ //bonuspoint=bonuspoint+45;
+ //console.log(bonuspoint);
+ let count=0;
+ function INCREAMENT(){
+    count=count+1;
+    console.log("count");
+ }
+ 
